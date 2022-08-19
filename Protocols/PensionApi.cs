832443fc5d461443659af81b833227b0662eb1c9
vamsi17120 <vamshi9848074790@gmail.com>
2022-08-19 09:22:20 +0530

    azure link update

--- Protocols/PensionApi.cs
+++ Protocols/PensionApi.cs
@@ -5,7 +5,7 @@
         public HttpClient Initial()
         {
             var client = new HttpClient();
-            client.BaseAddress = new Uri("https://localhost:44305/");
+            client.BaseAddress = new Uri("http://20.219.224.84/");
             return client;
         }
     }
